970c9a2ce437c95d1c7f8c1859e9386085ee403e
Mohitgaggar <gaggarmohit@gmail.com>
2019-10-25 17:56:31 +0530

    login feautures added

---
+++ SQL/sql.sql
@@ -0,0 +1,14 @@
+CREATE DATABASEIF NOT EXISTS panther;
+
+CREATE  TABLE IF NOT EXISTS `WT_USERS` (
+  `username` VARCHAR(150) NOT NULL ,
+  `password` VARCHAR(100) ,
+  PRIMARY KEY (`username`) )
+ENGINE = InnoDB;
+
+INSERT INTO `WT_USERS` (`username`,`password`)
+    VALUES ('admin','adminpass');
+INSERT INTO `WT_USERS` (`username`,`password`)
+    VALUES ('mohit','igotopes');
+
+SELECT * FROM `wt_users` WHERE username='mohit'
